d64e0aa78430edc47ade4ca04ffedc434890447a
Francisco Ferreira <manofrancisco@users.noreply.github.com>
2018-11-13 18:57:11 +0000

    Added shooting and healing

--- engine.py
+++ engine.py
@@ -1,21 +1,27 @@
 class Object:
-    x=1200
-    y=250
-    w=40
-    h=60
-    velocity = -50
-    color = (0,0,200)
-    def __init__(self):
-        print('Object created')
+    def __init__(self,x):
+        self.x=x
+        self.y=250
+        self.w=40
+        self.h=60
+        self.color = (0,0,200)
 
-    def move(self,energy):
+    def move(self,velocity):
         if(self.x  < - self.w):
             self.x = 1200
-
-        self.velocity = int(-15 * (energy+10)/100)
-        self.x = self.x + self.velocity
+        self.x = self.x + velocity
         
 
-
-
+class Projectile:
+    side = 15
+    def __init__(self,y_position):
+        self.x = 80
+        self.y = y_position +10  
+        self.v = 10
+        
+    def move(self):
+        if(self.x > 1000):
+            self.y = 0
+        else:
+            self.x += self.v 
     
--- game.py
+++ game.py
@@ -4,15 +4,26 @@ from engine import *
 
 
 def check_collision(obj):
-    if (obj.x < 90 ):
-        if(obj.y - obj.h < y_position):
+    if(obj.x < 90 and obj.x > 50 or obj.x+obj.w < 90 and obj.x+obj.w>50):
+        if(obj.y-obj.h < y_position):
             return True
     return False
+
+
+def check_projectile_collision(proj, obj):
+    if(obj.x <= proj.x):
+        if(proj.y > obj.y and proj.x < obj.x+obj.h):
+            return True
+    return False
+
+
 
 pygame.init()
 
 win = pygame.display.set_mode((1000,300))
-obj = Object()
+
+
+obj_coord = [2100,2500,2700,2800]
 
 clr = [127,127,127]
 
@@ -21,58 +32,32 @@ y_velocity = 0
 run = True
 jump_status = False
 
-energy = 70
+energy = 100
 counter = 0
+heal = False
+armed = False
+arming = False
+shoot = False
+projectiles = []
+objects = []
+velocity = -3
+distance_run = 0
+ending = obj_coord[-1]
 while run == True:
-    
+    if(distance_run > ending + 2500 ):
+        print("U win")
+        run = False
+    if(len(obj_coord)>0):
+        if(distance_run + 900 > obj_coord[0]):
+            objects.append(Object(obj_coord[0]))
+            obj_coord.pop(0)
+    distance_run -= velocity
+    print(distance_run)
+    velocity = int(-10 * (energy+10)/100)
     pygame.time.delay(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-
-    keys = pygame.key.get_pressed()
-
-    if(obj.x < 90 and obj.x > 50 or obj.x+obj.w < 90 and obj.x+obj.w>50):
-        if(obj.y-obj.h < y_position):
-            break
-
-    if(energy == 0):
-        print('U ded')
-        break
-    obj.move(energy)
-
-
-    if(counter == 20):
-        energy-= 1
-        counter = 0
-        print(energy)
-    else:
-        counter += 1
-
-    if keys[pygame.K_SPACE]:
-        if(not jump_status):
-            jump_status = True
-            y_velocity = 34
-    if keys[pygame.K_m]:
-        counter = 0
-        if(energy < 70):
-            energy += 30
-        else:
-            energy = 100
-
-    
-
-    if(jump_status):
-        y_velocity -= 2
-
-    y_position -= y_velocity
-
-    if(y_position >= 250):
-        jump_status = False
-        y_velocity = 0        
-    
-    
 
     clr[0] += random.randint(-6,6) if clr[0] > 6 and clr[0] < 249 else 0
     if(clr[0] < 6 or clr[0]>249):
@@ -88,11 +73,92 @@ while run == True:
     
 
     win.fill(tuple(clr))
-    pygame.draw.rect(win,obj.color,(obj.x,obj.y,obj.w,obj.h))
+
+    for obj in objects:
+        if(obj.x <= -20):
+            objects.remove(obj)
+        if check_collision(obj):
+            run = False
+        draw = True
+        for proj in projectiles:
+            if check_projectile_collision(proj,obj):
+                objects.remove(obj)
+                projectiles.remove(proj)
+                draw = False
+        if draw:
+            pygame.draw.rect(win,obj.color,(obj.x,obj.y,obj.w,obj.h))
+        obj.move(velocity)
+
+
+    if(energy == 0):
+        print('U ded')
+        break
+    #obj.move(energy)
+
+
+    if(counter == 50):
+        energy -= 1
+        counter = 0
+    else:
+        counter += 1
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_n]:
+        if(not arming):
+            arming = True
+            armed = True
+    else:
+        arming = False    
+
+        
+    if not armed:
+        if keys[pygame.K_SPACE]:
+            if(not jump_status):
+                jump_status = True
+                y_velocity = 34
+    else:
+        pygame.draw.rect(win,(255,0,0),(50,50,10,10))
+        if keys[pygame.K_m]:
+            if(not heal):
+                armed = False
+                heal = True
+                counter = 0
+                if(energy  > 70):
+                    energy = 100
+                else:
+                    energy += 30
+        else:
+            heal = False
+        if keys[pygame.K_b]:
+            if(not shooting):
+                shooting = True
+                armed = False
+                p = Projectile(y_position)
+                projectiles.append(p)
+        else: 
+            shooting = False
+
+
+
+    for p in projectiles:
+        p.move()
+        pygame.draw.rect(win,(127,0,0),(p.x,p.y,p.side,p.side,))
+
+    if(jump_status):
+        y_velocity -= 2
+
+    y_position -= y_velocity
+
+    if(y_position >= 250):
+        jump_status = False
+        y_velocity = 0        
+    
+    
     pygame.draw.rect(win,(50,200,80),(50,y_position,40,60))
     pygame.display.update()
 
-
+pygame.time.delay(1000)
 pygame.quit()
 
 
